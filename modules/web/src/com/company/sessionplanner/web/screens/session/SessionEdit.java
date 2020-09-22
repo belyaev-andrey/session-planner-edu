9d1836a8a0a2d0abd0b939d80b772bdf62e79e30
Andrey Belyaev <belyaev.andrey@gmail.com>
2020-09-22 13:24:48 +0300

    Added Vaadin Slider

--- modules/web/src/com/company/sessionplanner/web/screens/session/SessionEdit.java
+++ modules/web/src/com/company/sessionplanner/web/screens/session/SessionEdit.java
@@ -1,13 +1,44 @@
 package com.company.sessionplanner.web.screens.session;
 
+import com.haulmont.cuba.gui.components.Form;
+import com.haulmont.cuba.gui.components.HBoxLayout;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.sessionplanner.entity.Session;
+import com.vaadin.ui.AbstractOrderedLayout;
+import com.vaadin.ui.Slider;
+
+import javax.inject.Inject;
 
 @UiController("sessionplanner_Session.edit")
 @UiDescriptor("session-edit.xml")
 @EditedEntityContainer("sessionDc")
 @LoadDataBeforeShow
 public class SessionEdit extends StandardEditor<Session> {
+
+    private final Slider slider = new Slider();
+
+    @Inject
+    private HBoxLayout sliderBox;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        slider.setMin(0);
+        slider.setMax(4);
+        slider.setWidth("100%");
+
+        slider.addValueChangeListener( valueChangedEvent -> {
+            getEditedEntity().setDuration(valueChangedEvent.getValue().intValue());
+        });
+
+        sliderBox.unwrap(AbstractOrderedLayout.class).addComponent(slider);
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        slider.setValue(getEditedEntity().getDuration().doubleValue());
+    }
+
+
 
     @Subscribe
     public void onInitEntity(InitEntityEvent<Session> event) {
