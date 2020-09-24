d153dcd053a20eec55e553cd4b14c8d3db3cbffe
Andrey Belyaev <belyaev.andrey@gmail.com>
2020-09-24 11:21:34 +0300

    Added JavaMelody

--- modules/core/test/com/company/sessionplanner/SessionplannerTestContainer.java
+++ modules/core/test/com/company/sessionplanner/SessionplannerTestContainer.java
@@ -15,10 +15,11 @@ public class SessionplannerTestContainer extends TestContainer {
         super();
         appComponents = Arrays.asList(
                 "com.haulmont.cuba",
-                "com.haulmont.addon.restapi",
                 "com.haulmont.addon.helium",
                 "com.haulmont.reports",
-                "com.haulmont.addon.zookeeper");
+                "com.haulmont.addon.zookeeper",
+                "com.haulmont.addon.restapi",
+                "com.haulmont.addon.cubajm");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
